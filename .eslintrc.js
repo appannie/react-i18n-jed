87c47b971a62ec902f0a4a948a586b9724cf0a13
Simon Boudrias <admin@simonboudrias.com>
2017-12-14 16:21:10 +0800

    Fix build (#5)

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,10 +34,7 @@ module.exports = {
         'import/no-extraneous-dependencies': [
             'error',
             {
-                devDependencies: [
-                    '**/test.js',
-                    '**/*.test.js',
-                ],
+                devDependencies: ['jest.setup.js', '**/test.js', '**/*.test.js'],
             },
         ],
         'prettier/prettier': [
