5ce60a9fa1d37b408e78ed95fcb4d9adb02db59e
wxu <wxu@appannie.com>
2018-05-13 04:02:59 +0800

    Remove component function

--- src/I18nProvider.js
+++ src/I18nProvider.js
@@ -4,21 +4,10 @@ import { type I18nType } from '.';
 
 const I18nContext = React.createContext();
 
-/* eslint-disable react/prefer-stateless-function */
-class I18nProvider extends React.Component<{
-    i18n: I18nType,
-    children?: React.Node,
-}> {
-    render() {
-        return (
-            <I18nContext.Provider value={this.props.i18n}>
-                {this.props.children}
-            </I18nContext.Provider>
-        );
-    }
-}
+const I18nProvider = (props: { i18n: I18nType, children?: React.Node }) => (
+    <I18nContext.Provider value={props.i18n}>{props.children}</I18nContext.Provider>
+);
 
 export { I18nContext };
 
 export default I18nProvider;
-/* eslint-enable react/prefer-stateless-function */
